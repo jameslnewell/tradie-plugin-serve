04e94919371a0e6a3c6b18d1dd5dbb6bda530706
James Newell <james@digitaledgeit.com.au>
2016-08-12 07:53:17 +1000

    upgrade to work with tradie v1.0.0

--- index.js
+++ index.js
@@ -10,39 +10,34 @@ const dbg = debug('tradie-plugin-serve');
 export default (options = {}) => tradie => {
   const {host, port = 5000} = options;
 
-  const init = cmd => {
+  //only run the server while we're watching
+  if (!tradie.watch) {
+    return;
+  }
 
-    //only run the server while we're watching
-    if (!cmd.args.watch) {
-      return;
-    }
+  //only run the server while we're building
+  if (tradie.command !== 'build') {
+    return;
+  }
 
-    //only run the server while we're building
-    if (cmd.name !== 'build') {
-      return;
-    }
+  //configure the server
+  const server = factory.create({
+    path: tradie.dest,
+    verbose: false,
+    host,
+    port,
+    fallback: '/index.html'
+  });
+  //TODO: use express-http-proxy or http-proxy-middleware to proxy requests to an API and avoid CORS
 
-    //configure the server
-    const server = factory.create({
-      path: tradie.config.dest,
-      verbose: false,
-      host,
-      port,
-      fallback: '/index.html'
-    });
-    //TODO: use express-http-proxy or http-proxy-middleware to proxy requests to an API and avoid CORS
+  //start the server when we start watching
+  server.start(() => dbg(`Started server at http://${host ? host : 'localhost'}:${port}`)); //TODO:
+  // handle
+  // errors
 
-    //start the server when we start watching
-    server.start(() => dbg(`Started server at http://${host ? host : 'localhost'}:${port}`)); //TODO:
-    // handle
-    // errors
+  //stop the server when we stop watching
+  tradie.once('command.finished', () => {
+    server.stop(() => dbg(`Stopped server at http://${host ? host : 'localhost'}:${port}`)); //TODO: handle errors
+  });
 
-    //stop the server when we stop watching
-    tradie.once('command.finished', () => {
-      server.stop(() => dbg(`Stopped server at http://${host ? host : 'localhost'}:${port}`)); //TODO: handle errors
-    });
-
-  };
-
-  tradie.once('command.started', init);
 };
